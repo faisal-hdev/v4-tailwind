482301be12229e73916ab318cb52e69c39e9c46d
faisal-hdev <itsfaisalchowdhury@gmail.com>
2025-01-28 05:14:15 +0600

    Nothing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ function App() {
   return (
     <>
       <div className="p-10 text-center">
-        <h1 className="text-6xl text-black font-medium monoton">Faisal hDev</h1>
+        <h1 className="text-6xl text-black font-medium monoton">
+          Faisal. hDev
+        </h1>
         <p className="poppins text-3xl font-extralight my-4 ">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia
           inventore dolorum placeat aliquam sequi saepe consequatur vel.
           Laudantium sunt sed ab pariatur?
         </p>
-        <h1 className="text-6xl text-black font-medium monoton">Faisal hDev</h1>
+        <h1 className="text-6xl text-black font-medium monoton">
+          Faisal. hDev
+        </h1>
       </div>
     </>
   );
